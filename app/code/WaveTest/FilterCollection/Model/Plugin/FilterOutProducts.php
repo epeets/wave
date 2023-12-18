dd83464787478814a4eb7d2a68f463d441050f53
epeets <epeets@gmail.com>
2023-12-18 10:50:01 -0600

    Don't loop product repo in foreach

--- app/code/WaveTest/FilterCollection/Model/Plugin/FilterOutProducts.php
+++ app/code/WaveTest/FilterCollection/Model/Plugin/FilterOutProducts.php
@@ -8,21 +8,27 @@
 
 namespace WaveTest\FilterCollection\Model\Plugin;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\OpenSearch\Model\SearchClient;
 
 class FilterOutProducts
 {
     private ProductRepositoryInterface $productRepository;
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
 
     /**
      * @param ProductRepositoryInterface $productRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
      */
     public function __construct(
-        ProductRepositoryInterface $productRepository
+        ProductRepositoryInterface $productRepository,
+        SearchCriteriaBuilder      $searchCriteriaBuilder
     )
     {
         $this->productRepository = $productRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
     }
 
     /**
@@ -37,28 +43,48 @@ class FilterOutProducts
         }
 
         $hits = $result['hits']['hits'];
+        $resultProductIds = [];
         foreach ($hits as $key => $hit) {
             $productId = $hit['fields']['_id'][0];
-            if ($this->priceLimit($productId)) {
-                unset($result['hits']['hits'][$key]);
+            $resultProductIds[] = $productId;
+        }
+
+        $overLimitProducts = $this->priceLimit($resultProductIds);
+
+        if ($overLimitProducts) {
+            foreach ($hits as $key => $hit) {
+                $productId = $hit['fields']['_id'][0];
+                if (in_array($productId, $overLimitProducts) !== false) {
+                    unset($result['hits']['hits'][$key]);
+                }
             }
         }
+
         return $result;
     }
 
     /**
-     * @param $id
-     * @return bool
+     * @param $resultProductIds
+     * @return array|null
      */
-    private function priceLimit($id): bool
+    private function priceLimit($resultProductIds): ?array
     {
-        try {
-            return $this->productRepository->getById($id)->getPrice() >= 100.00;
-        } catch (\Exception $e) {
-            // Do nothing.
+        $productSearchCriteria = $this->searchCriteriaBuilder->addFilter('entity_id', $resultProductIds, 'in')
+            ->addFilter('price', 100.00, 'qteq')
+            ->create();
+
+        $collection = $this->productRepository->getList($productSearchCriteria)->getItems();
+
+        if (count($collection) >= 0) {
+            $ids = [];
+            foreach ($collection as $product) {
+                $ids[] = $product->getId();
+            }
+
+            return $ids;
         }
 
-        return false;
+        return null;
     }
 
 }
